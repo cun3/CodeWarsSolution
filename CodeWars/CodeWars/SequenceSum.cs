0a1835dab21d145ebfaff61c8435ea06b72e80fc
cun3 <nachiketh1989@gmail.com>
2016-06-25 21:59:22 -0400

    Added solution for sequence sum.

---
+++ CodeWars/CodeWars/SequenceSum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using NUnit.Framework;
+
+//Sum of 'n' Numbers
+//sum_of_n(or SequenceSum.sumOfN in Java, SequenceSum.SumOfN in C#) takes an integer n and returns a List (an Array in Java/C#) of length abs(n) + 1. The List/Array contains the numbers in the arithmetic series produced by taking the sum of the consecutive integer numbers from 0 to n inclusive.
+//n can also be 0 or a negative value.
+
+namespace CodeWars
+{
+    public class SequenceSum
+    {
+        public static int[] SumOfN(int n)
+        {
+            int m = Math.Abs(n);
+            int[] numbers = new int[m + 1];
+            int sum = 0;
+            int num = 0;
+
+            if (n > 0)
+            {
+                for (int i = 0; i <= m; i++)
+                {
+                    sum += i;
+                    numbers[i] = sum;
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= m; i++)
+                {
+                    sum += i;
+                    num = sum - (2 * sum);
+                    numbers[i] = num;
+                }
+            }
+            return numbers;
+            //throw new System.NotImplementedException();
+        }
+    }
+
+}
+
